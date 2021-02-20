3a4d39004780dd751e5440749a74501e67671d58
Michael Pearson <michaelpearsun@gmail.com>
2021-02-19 20:57:21 -0600

    Selected Song

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import LibrarySong from './LibrarySong';
+import Song from './Song';
 
-const Library = ({songs}) => {
+const Library = ({songs, setCurrentSong }) => {
     return(
         <div className="library">
             <h2>Library</h2>
             <div className="library-songs">
                 {songs.map((song) => (
-                <LibrarySong song={song} />
+                <LibrarySong 
+                    id={song.id}
+                    songs={songs}
+                    song={song} 
+                    setCurrentSong={setCurrentSong} 
+                    key={song.id}
+                />
                 ))}
             </div>
         </div>
